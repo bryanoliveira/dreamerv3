fb8237fb75b99c953c0031577ed04f83079be282
Bryan Lincoln <bryanufg@gmail.com>
2024-04-27 18:06:15 -0300

    fix: update experiment script

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -3,4 +3,4 @@
 logdir=$1
 mkdir -p "$logdir"
 shift
-python dreamerv3/train.py --logdir "$logdir" "$@" 2>&1 | tee "$logdir/logs.txt"
+python dreamerv3/main.py --logdir "$logdir" "$@" 2>&1 | tee "$logdir/logs.txt"

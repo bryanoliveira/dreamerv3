519df80bd84bce49ea2be49f248a93ea5eaf323e
Bryan Lincoln <bryanufg@gmail.com>
2024-05-09 10:09:59 -0300

    chore: add logs

--- embodied/run/train.py
+++ embodied/run/train.py
@@ -99,12 +99,11 @@ def train(make_agent, make_replay, make_env, make_logger, args):
   checkpoint.load_or_save()
   should_save(step)  # Register that we just saved.
 
-  agent.apply_stop_gradients()
-
   print('Start training loop')
   policy = lambda *args: agent.policy(
       *args, mode='explore' if should_expl(step) else 'train')
   driver.reset(agent.init_policy)
+  agent.apply_stop_gradients()
   while step < args.steps:
 
     driver(policy, steps=10)

f61857918786a164833e7fb4a3c0c7a8cef6a491
Bryan Lincoln <bryanufg@gmail.com>
2024-05-22 17:59:53 -0300

    fix: update logdir path

--- experiments/3.running/pacman100k_cont_wm.sh
+++ experiments/3.running/pacman100k_cont_wm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for seed in 176 594 907; do
-    bash experiments/experiment.sh ./logdir/20240514_110000-mspacman_100k_${seed}_cont_wm_frozen --configs atari100k --seed ${seed} --task atari_ms_pacman --run.load_keys '^agent/(enc|dec|dyn|rew|con)' --run.load_skip_keys '^(replay|step)'
+    bash experiments/experiment.sh ./logdir/20240522_180000-mspacman_100k_${seed}_cont_wm --configs atari100k --seed ${seed} --task atari_ms_pacman --run.load_keys '^agent/(enc|dec|dyn|rew|con)' --run.load_skip_keys '^(replay|step)'
 done
 

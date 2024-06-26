122832ca4d37700a0d898f0272e4782f6627e163
Bryan Lincoln <bryanufg@gmail.com>
2024-06-26 14:31:36 -0300

    chore: remove experiments folder

--- experiments/4.executed/pacman100k.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for seed in 176 594 907; do # 267 769
-    bash experiments/experiment.sh ./logdir/$(date "+%Y%m%d_%H%M%S")-pacman_100k_$seed --configs atari100k --seed $seed --task atari_ms_pacman
-done
-
